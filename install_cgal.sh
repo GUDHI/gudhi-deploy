f71c3749c4d57afcb9747fe99a8cfbb8aa960b6a
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2021-01-16 11:01:35 +0100

    Moved dockerfiles and requirements from gudhi-devel

---
+++ install_cgal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# error management : https://dev.to/banks/stop-ignoring-errors-in-bash-3co5
+set -ue
+
+export CGAL_VERSION="5.2"
+
+curl -LO "https://github.com/CGAL/cgal/releases/download/v${CGAL_VERSION}/CGAL-${CGAL_VERSION}.tar.xz"
+tar xf CGAL-${CGAL_VERSION}.tar.xz
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ../CGAL-${CGAL_VERSION}/
+make install
+cd ..
+rm -rf build CGAL-${CGAL_VERSION}
