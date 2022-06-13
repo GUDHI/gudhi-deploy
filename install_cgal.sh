fc3ebce6e9a8ac6d993366c324670f2cba68128b
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2022-06-13 16:46:57 +0200

    Update ubuntu 22, cgal 5.4.1

--- install_cgal.sh
+++ install_cgal.sh
@@ -3,7 +3,7 @@
 # error management : https://dev.to/banks/stop-ignoring-errors-in-bash-3co5
 set -ue
 
-export CGAL_VERSION="5.3.1"
+export CGAL_VERSION="5.4.1"
 
 curl -LO "https://github.com/CGAL/cgal/releases/download/v${CGAL_VERSION}/CGAL-${CGAL_VERSION}.tar.xz"
 tar xf CGAL-${CGAL_VERSION}.tar.xz
